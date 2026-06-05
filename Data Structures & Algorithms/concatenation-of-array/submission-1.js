1f045d64a6c858f088832124e9125a5056e7b61e
Felipe Sakai Ipolito <167255155+FelipeSakai@users.noreply.github.com>
2026-06-05 08:14:23 -0400

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[]}
+     */
+    getConcatenation(nums) {
+        let arr = [];
+
+        for (let i = 0; i < nums.length * 2; i++) {
+            arr[i] = nums[i % nums.length];
+        }
+        return arr;
+    }
+}
